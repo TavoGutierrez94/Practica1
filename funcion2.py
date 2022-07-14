6452132e91983fe27b2947295646b23215a34313
TavoGutierrez94 <tavo.gtz31@gmail.com>
2022-07-13 19:16:03 -0500

    Crear archivo funcion2.oy
    Se crea el archivo para validar que un numero ingresado sea mayor a 18 por TavoGtz94

---
+++ funcion2.py
@@ -0,0 +1,8 @@
+def numero():
+    num = int(input("Ingresa valor: "))
+    if num >= 18:
+        print("El numero " + str(num) + " es mayor o igual a 18")
+    else:
+        print("NNo es mayor a 18")
+
+numero()
